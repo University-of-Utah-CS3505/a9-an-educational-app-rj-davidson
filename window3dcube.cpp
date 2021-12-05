5b34f3421443e3955429be8c1473b5cc4d6e95c2
Elizabeth Lukner <elukner@gmail.com>
2021-12-05 16:37:22 -0700

    2d cube now looks like a real 2d rubiks cube

--- window3dcube.cpp
+++ window3dcube.cpp
@@ -22,12 +22,12 @@ createTopOfCube();
 
 
 
-   scene1->setSceneRect(0, 0 , 500, 300);
+  // scene1->setSceneRect(0, 0 , 500, 300);
    scene1->setBackgroundBrush(Qt::white);
 
 
     ui->cubeGraphicsView->setScene(scene1);
-    ui->cubeGraphicsView->scale(-1,1);
+    ui->cubeGraphicsView->scale(1,-1);
 
 
 
@@ -60,29 +60,42 @@ void Window3DCube::createLeftOfCube(){
 
     for(int xPos = 0; xPos<3; xPos++)
     {
+        int xPosCorrected = 2-xPos; //change xPosition so that 0 element is on left and 2 element is on right.
+                                    //This ensures the face 0,0 point is on the bottom left corner for every face
         for(int yPos = 0; yPos<3; yPos++)
         {
-            xPos = -xPos+2; //change xPosition so that 0 element is on left and 2 element is on right.  This ensures the face 0,0 point is on the bottom left corner
-            point1x = -eSize*xPos*xScale;
-            point1y = eSize*yPos;
-            point2x = -eSize*(xPos+1)*xScale;
-            point2y = eSize*yPos+eSize*yScale;
-            point3x = -eSize*(xPos+1)*xScale;
-            point3y = eSize*(yPos+1)+eSize*yScale;
-            point4x = -eSize*xPos*xScale;
-            point4y = eSize*(yPos+1);
+            point1x = -eSize*xPosCorrected*xScale;
+            point1y = eSize*yPos+eSize*xPosCorrected*yScale;
+            point2x = -eSize*(xPosCorrected+1)*xScale;
+            point2y = eSize*yPos+eSize*yScale+eSize*xPosCorrected*yScale;
+            point3x = -eSize*(xPosCorrected+1)*xScale;
+            point3y = eSize*(yPos+1)+eSize*yScale+eSize*xPosCorrected*yScale;
+            point4x = -eSize*xPosCorrected*xScale;
+            point4y = eSize*(yPos+1)+eSize*xPosCorrected*yScale;
+
+            QPainterPath path1;
 
             path1.moveTo(point1x, point1y);
             path1.lineTo(point2x, point2y);
             path1.lineTo(point3x, point3y);
             path1.lineTo(point4x, point4y);
             path1.lineTo(point1x, point1y);
-            itemRLeft = scene1->addPath(path1);
-            itemRLeft->setBrush(Qt::yellow);
-            vector.append(itemRLeft);
-            qDebug()<<vector.size();
+            QGraphicsPathItem *tempItemReference;
+            tempItemReference = scene1->addPath(path1);
+            vector.append(tempItemReference);
+
+//            if(xPos == 0){
+//                tempItemReference->setBrush(Qt::green);
+//            }else if(xPos == 2){
+//                tempItemReference->setBrush(Qt::yellow);
+//            }else{
+//                tempItemReference->setBrush(Qt::black);
+//            }
+//            tempItemReference->setPen(QPen(Qt::red));
+            tempItemReference->setBrush(Qt::yellow);
         }
     }
+
 
 }
 
@@ -95,30 +108,30 @@ void Window3DCube::createRightOfCube(){
         for(int yPos = 0; yPos<3; yPos++)
         {
             point1x = eSize*xPos*xScale;
-            point1y = eSize*yPos;
+            point1y = eSize*yPos+eSize*xPos*yScale;
             point2x = eSize*(xPos+1)*xScale;
-            point2y = eSize*yPos+eSize*yScale;
+            point2y = eSize*yPos+eSize*yScale+eSize*xPos*yScale;
             point3x = eSize*(xPos+1)*xScale;
-            point3y = eSize*(yPos+1)+eSize*yScale;
+            point3y = eSize*(yPos+1)+eSize*yScale+eSize*xPos*yScale;
             point4x = eSize*xPos*xScale;
-            point4y = eSize*(yPos+1);
+            point4y = eSize*(yPos+1)+eSize*xPos*yScale;
+
+            QPainterPath path2;
 
             path2.moveTo(point1x, point1y);
             path2.lineTo(point2x, point2y);
             path2.lineTo(point3x, point3y);
             path2.lineTo(point4x, point4y);
             path2.lineTo(point1x, point1y);
-            itemRRight = scene1->addPath(path2);
-            itemRRight->setBrush(Qt::red);
-            vector.append(itemRRight);
-            qDebug()<<vector.size();
+
+            QGraphicsPathItem *tempItemReference;
+            tempItemReference = scene1->addPath(path2);
+            vector.append(tempItemReference);
+
+            tempItemReference->setBrush(Qt::red);
         }
 
     }
-
-
-
-save2DCubeXY();
 }
 
 /*
@@ -134,22 +147,27 @@ void Window3DCube::createTopOfCube(){
             point1y = cubeSize+(xPos+yPos)*eSize*yScale;
             point2x = (xPos+1-yPos)*eSize*xScale;
             point2y = cubeSize+(xPos+yPos)*eSize*yScale+eSize*yScale;
-            point3x = (xPos+1-yPos)*eSize*xScale;
+            point3x = (xPos-yPos)*eSize*xScale;
             point3y = cubeSize+(xPos+yPos+1)*eSize*yScale+eSize*yScale;
             point4x = (xPos-1-yPos)*eSize*xScale;
             point4y = cubeSize+(xPos+yPos)*eSize*yScale+eSize*yScale;
+
+            QPainterPath path3;
 
             path3.moveTo(point1x, point1y);
             path3.lineTo(point2x, point2y);
             path3.lineTo(point3x, point3y);
             path3.lineTo(point4x, point4y);
             path3.lineTo(point1x, point1y);
-            itemRTop = scene1->addPath(path3);
-            itemRTop->setBrush(Qt::green);
-            vector.append(itemRTop);
+
+
+            QGraphicsPathItem *tempItemReference;
+            tempItemReference = scene1->addPath(path3);
+            vector.append(tempItemReference);
+
+            tempItemReference->setBrush(Qt::green);
         }
     }
-    qDebug()<<vector.size();
 
 }
 
