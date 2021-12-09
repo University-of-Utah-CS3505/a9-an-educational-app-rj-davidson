aa7664a323b5a9df344c5e7ca4cd012405e142b0
Orions99 <u1172683@umail.utah.edu>
2021-12-09 15:14:16 -0700

    Initial box2D celebration setup

--- RubiksCelebration.h
+++ RubiksCelebration.h
@@ -4,7 +4,6 @@
 #include <QWidget>
 
 #include <box2d/box2d.h>
-
 #include <QKeyEvent>
 #include <QPaintEvent>
 #include <QPainter>
@@ -20,27 +19,16 @@ public:
 
 signals:
 
+protected:
+    void paintEvent(QPaintEvent* event) override;
+    void keyPressEvent(QKeyEvent* event) override;
+
+private:
+    QPixmap pm;
+    b2World world;
+    b2Body* ground;
+    b2Body* cube;
+    QTimer worldTimer;
 };
 
 #endif // RUBIKSCELEBRATION_H
-
-
-
-//class LabRubiksWidget : public QWidget {
-//  Q_OBJECT
-// public:
-//  explicit LabRubiksWidget(QWidget* parent = nullptr);
-
-// signals:
-
-// protected:
-//  void paintEvent(QPaintEvent* event) override;
-//  void keyPressEvent(QKeyEvent* event) override;
-
-// private:
-//  QPixmap pm;
-//  b2World world;
-//  b2Body* ground;
-//  b2Body* cube;
-//  QTimer worldTimer;
-//};
